fa6027b0c6a3ef3da731031e6c33dc1ec33078c3
17robots <mdray@ameritech.net>
2025-03-07 09:40:55 -0500

    feat: update highlighting queries to match neovim

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,32 +1,51 @@
-(declaration (identifier) @variable)
-(mut_declaration (identifier) @variable)
-
 ["defer" "comp" "undefined" "null"] @keyword
-"return" @keyword.control.return
-"use" @keyword.control.import
-["if" "match" "else"] @keyword.control.conditional
+["if" "match" "else"] @keyword.conditional
 ["for" "while"] @keyword.control.repeat
 ["try" "catch"] @keyword.control.exception
 ["pub" "inline" "mut"] @keyword.storage.modifier
-["struct" "enum" "error"] @type.builtin
-"type" @type.parameter
+["true" "false"] @constant.builtin.boolean
+["struct" "enum" "error" "type"] @type.builtin
+["module" "use"] @keyword.control.import
+"return" @keyword.control.return
 
 (identifier) @variable
+
+(declaration (identifier) @type (expression (non_literal_expression [(enum) (error) (struct)])))
+(declaration (identifier) @function (expression (non_literal_expression (fn))))
+
+(mut_declaration (identifier) @type (expression (non_literal_expression [(enum) (error) (struct)])))
+(mut_declaration (identifier) @function (expression (non_literal_expression (fn))))
+
+((identifier) @type.builtin (#match? @type "^(i|u)[0-9]+$"))
+((identifier) @type.builtin (#match? @type "^f(16|32|64|128)+$"))
+
 (comment) @comment
+
 (block (identifier) @label)
 (return_expression (identifier) @label)
 (break_expression (identifier) @label)
 (continue_expression (identifier) @label)
 (member_access (identifier) @variable.other.member)
-(fn (identifier) @variable.parameter)
 (enum_error_initialization (identifier) @type.enum.variant)
+(call (non_literal_expression (identifier))) @function.call
 
 (string) @string
 (number) @constant.numeric.integer
 (float) @constant.numeric.float
 (char) @constant.character
 
+(enum_member (identifier) @constant)
+(error_member (identifier) @constant)
+(struct_member (identifier) @variable.other.member)
+
+(enum_member (declaration (identifier) @variable.other))
+(error_member (declaration (identifier) @variable.other))
+(struct_member (declaration (identifier) @variable.other))
+
+(struct_initialization name: (identifier) @type)
+
 ["," "." ":"] @punctuation.delimiter
 ["(" ")" "[" "]" "{" "}"] @punctuation.bracket
 
 ["=" "*=" "%=" "/=" "+=" "-=" "<<=" ">>=" "&=" "^=" "|=" "||" "&&" "==" "!=" ">" ">=" "<" "<=" "&" "|" "<<" ">>" "+" "-" "*" "/" "%"] @operator
+
